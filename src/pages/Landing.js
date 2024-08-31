6ca78c9e760793f8d15b8ffb98453c222b04f52f
TecVit <vitorfreelancer021@gmail.com>
2024-08-31 14:45:39 -0300

    atualizando com cursores de imagens

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -4,7 +4,7 @@ import '../css/Landing.css';
 import Logo from '../icons/manage.png';
 
 // Icones
-import { IoIosArrowForward, IoIosCheckmark } from "react-icons/io";
+import { IoIosArrowDown, IoIosArrowForward, IoIosCheckmark, IoIosLink } from "react-icons/io";
 import { FcGoogle } from 'react-icons/fc';
 import { useNavigate } from 'react-router-dom';
 import { FaGithub, FaInstagram, FaWhatsapp } from 'react-icons/fa';
@@ -28,6 +28,27 @@ export default function Landing() {
     })
   }
 
+  const [mdStatusTeam, setMdStatusTeam] = useState([false, false, false, false]);
+  const status = [
+    'Admin',
+    'Moderador',
+    'Editor',
+    'Contribuidor',
+    'Leitor',
+  ];
+  const [statusSelected, setStatusSelected] = useState(status[0]);
+  const handleStatusTeam = (i) => {
+    setMdStatusTeam((status) => {
+        const list = status.map((val, index) => {
+            if (index === i) {
+                return !val;
+            }
+        })
+        console.log(list);
+        return list;
+    })
+  }
+
   return (
     <main className='container-landing'>
         <section className='content-landing'>
@@ -38,7 +59,7 @@ export default function Landing() {
                     <img onClick={() => window.location.href = "/"} className='logo' src={Logo} />
                     <div className='links'>
                         <a href='/#initial'>Início</a>
-                        <a href='/#functions'>Funções</a>
+                        <a href='/#features'>Funcionalidades</a>
                         <a href='/#examples'>Exemplos</a>
                         <a href='/#updates'>Atualizações</a>
                     </div>
@@ -62,7 +83,7 @@ export default function Landing() {
             </div>
 
             {/* Products */}
-            <div id='functions' className='container-products'>
+            <div id='features' className='container-products'>
                 <div className='content-products'>
 
                     {/* Join Workspace */}
@@ -117,26 +138,30 @@ export default function Landing() {
                         <p>Manage School possui um gerenciamento personalizado</p>
                         <div className='modal'>
                             <div className='form'>
-                                <h1>New workspace</h1>
-                                <p>Create a new workspace for your management</p>
+                                <h1>Novo espaço de trabalho</h1>
+                                <p>Crie um novo espaço de trabalho para seu gerenciamento</p>
                                 <div className='linha'></div>
                                 <div className='input'>
-                                    <label>Workspace Name</label>
-                                    <input placeholder='Enter the workspace name' type='text' />
+                                    <label>Nome do espaço de trabalho</label>
+                                    <input placeholder='ex: Biblioteca Escolar 2024' type='text' />
                                 </div>
                                 <div className='checkbox'>
-                                    <div onClick={() => handleCreateWorkspaceCheckbox(0)} className={`input-checkbox ${createWorkspaceCheckbox[0] && 'selecionado'}`}></div>
-                                    <div>
-                                        <h1>Create a private workspace</h1>
-                                        <p>As a private workspace, only admins could rewrite</p>
+                                    <div tabIndex={0} onKeyDown={(event) => event.key === "Enter" && handleCreateWorkspaceCheckbox(0)} onClick={() => handleCreateWorkspaceCheckbox(0)} className={`input-checkbox ${createWorkspaceCheckbox[0] && 'selecionado'}`}></div>
+                                    <div className='text'>
+                                        <h1>Privado</h1>
+                                        <p>Por ser um espaço de trabalho privado, somente administradores podem edita-lo</p>
                                     </div>
                                 </div>
                                 <div className='checkbox'>
-                                    <div onClick={() => handleCreateWorkspaceCheckbox(1)} className={`input-checkbox ${createWorkspaceCheckbox[1] && 'selecionado'}`}></div>
-                                    <div>
-                                        <h1>Create a public workspace</h1>
-                                        <p>As a public workspace, everyone could rewrite</p>
+                                    <div tabIndex={0} onKeyDown={(event) => event.key === "Enter" && handleCreateWorkspaceCheckbox(1)} onClick={() => handleCreateWorkspaceCheckbox(1)} className={`input-checkbox ${createWorkspaceCheckbox[1] && 'selecionado'}`}></div>
+                                    <div className='text'>
+                                        <h1>Público</h1>
+                                        <p>Por ser um espaço de trabalho público, todos podem ter acesso</p>
                                     </div>
+                                </div>
+                                <div className='textarea'>
+                                    <label>Descrição</label>
+                                    <textarea placeholder='Adicione sua descrição para o espaço de trabalho'></textarea>
                                 </div>
                             </div>
                             
@@ -144,6 +169,53 @@ export default function Landing() {
                             <div className='btns'>
                                 <button className='btn-cancel'>Cancel</button>
                                 <button className='btn-create'>Create</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* Invite Team - Part 2 */}
+                    <div className='create-workspace'>
+                        <div className='modal'>
+                            <div className='form'>
+                                <h1>Seu Time</h1>
+                                <p>Convide seu time para te ajudar no Manage School</p>
+                                <div className='send'>
+                                    <div className='input-with-select'>
+                                        <input placeholder='Email' type='text' />
+                                        <div className='select'>
+                                            <button onClick={() => handleStatusTeam(0)}>
+                                                {statusSelected}
+                                                <IoIosArrowDown className='icon' />
+                                            </button>
+                                            {mdStatusTeam[0] && (
+                                                <div className='list'>
+                                                    {status.length > 0 && (
+                                                        status.map((val, index) => (
+                                                            <button onClick={() => {
+                                                                handleStatusTeam(0);
+                                                                setStatusSelected(val);
+                                                            }}>
+                                                                <p>{val}</p>
+                                                            </button>
+                                                        ))
+                                                    )}
+                                                </div>
+                                            )}
+                                        </div>
+                                    </div>
+                                    <button>Adicionar</button>
+                                </div>
+                                <div className='response-send'>
+
+                                </div>
+                            </div>
+                            
+                            {/* Buttons */}
+                            <div className='links'>
+                                <button className='copiar'>
+                                    <IoIosLink className='icon' />
+                                    Copiar Link
+                                </button>
                             </div>
                         </div>
                     </div>
